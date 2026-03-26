2cbc58e071876d7d4875f615bb780ea28f676197
Federico Brighi <federico.brighi2@studio.unibo.it.>
2026-03-26 11:03:02 +0100

    Done Task1

--- src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
@@ -1,5 +1,9 @@
 package it.unibo.pps.tasks.adts
 
+import it.unibo.pps.u03.Sequences.Sequence
+
+import scala.NamedTuple.Head
+
 /*  Exercise 1: 
  *  Complete the implementation of ComplexADT trait below, so that it passes
  *  the test in ComplexTest.
@@ -19,12 +23,25 @@ object Ex1ComplexNumbers:
 
   object BasicComplexADT extends ComplexADT:
 
+    case class ComplexImpl(realPart: Double, imaginaryPart: Double)
+    
+    import ComplexImpl.*  
+    
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    
+    opaque type Complex = ComplexImpl
+    
+    def complex(re: Double, im: Double): Complex = ComplexImpl(re,im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.realPart
+      def im(): Double = complex.imaginaryPart
+      def sum(other: Complex): Complex = 
+        ComplexImpl(complex.realPart + other.realPart, complex.imaginaryPart + other.imaginaryPart)
+      def subtract(other: Complex): Complex =
+        ComplexImpl(complex.realPart - other.realPart, complex.imaginaryPart - other.imaginaryPart)
+      def asString(): String =
+        if complex.imaginaryPart == 0 then complex.realPart + ""
+        else if complex.realPart != 0 && complex.imaginaryPart < 0 then complex.realPart + " - " + -complex.imaginaryPart + "i"
+        else if complex.realPart == 0 then complex.imaginaryPart + "i"
+        else complex.realPart + " + " + complex.imaginaryPart + "i"
+          
