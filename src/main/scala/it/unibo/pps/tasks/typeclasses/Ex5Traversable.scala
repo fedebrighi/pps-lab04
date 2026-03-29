b373020957cc814fffbb3c6d986a7a011176aef0
Federico Brighi <federico.brighi2@studio.unibo.it.>
2026-03-29 14:30:12 +0200

    Done Task5

--- src/main/scala/it/unibo/pps/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/it/unibo/pps/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,7 @@
 package it.unibo.pps.tasks.typeclasses
 
 import it.unibo.pps.u03.Sequences.Sequence, Sequence.*
+import it.unibo.pps.u03.Optionals.Optional, Optional.*
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -10,17 +11,46 @@ import it.unibo.pps.u03.Sequences.Sequence, Sequence.*
  *    "consumer function" on all elements (with type A) of a datastructure T[A] 
  *    Note Traversable is a 2-kinded trait (similar to Filterable, or Monad)
  *  - Write givens for Traversable[Optional] and Traversable[Sequence]
- *  - Show you can use the generalisation of logAll to:
+ *  - Show you can use the generalization of logAll to:
  *  -- log all elements of an Optional, or of a Traversable
  *  -- println(_) all elements of an Optional, or of a Traversable
  */
 
 object Ex5Traversable:
 
-  def log[A](a: A): Unit = println("The next element is: "+a)
+  trait Traversable[T[_]]:
+    def traverse[A](t: T[A])(f: A => Unit): Unit
+
+  given Traversable[Sequence] with
+    def traverse[A](seq: Sequence[A])(f: A => Unit): Unit = seq match
+      case Cons(h, t) => f(h); traverse(t)(f)
+      case _ => ()
+
+  given Traversable[Optional] with
+    def traverse[A](opt: Optional[A])(f: A => Unit): Unit = opt match
+      case Just(a) => f(a)
+      case _ => ()
+
+  def log[A](a: A): Unit = println("The next element is: " + a)
 
   def logAll[A](seq: Sequence[A]): Unit = seq match
     case Cons(h, t) => log(h); logAll(t)
     case _ => ()
 
-  
+  def generalizedLogAll[T[_]: Traversable, A](t: T[A])(f: A => Unit): Unit =
+    val traversable = summon[Traversable[T]]
+    traversable.traverse(t)(f)
+
+@main def tryTraversables(): Unit =
+  import Ex5Traversable.*
+
+  val si = Cons(10, Cons(20, Cons(30, Nil())))
+  val opt1 = Just(1)
+  val opt2 = Empty()
+
+  generalizedLogAll(si)(log)
+  generalizedLogAll(si)(println(_))
+  generalizedLogAll(opt1)(log)
+  generalizedLogAll(opt1)(println(_))
+  generalizedLogAll(opt2)(log)
+  generalizedLogAll(opt2)(println(_))
