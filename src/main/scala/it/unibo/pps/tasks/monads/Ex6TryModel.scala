072f868db098d701bbede7be96b9588a2a9015b3
Federico Brighi <federico.brighi2@studio.unibo.it.>
2026-03-29 21:10:44 +0200

    Done Task6

--- src/main/scala/it/unibo/pps/tasks/monads/Ex6TryModel.scala
+++ src/main/scala/it/unibo/pps/tasks/monads/Ex6TryModel.scala
@@ -31,10 +31,12 @@ object Ex6TryModel:
       case TryImpl.Failure(_) => other
 
   given Monad[Try] with
-    override def unit[A](value: A): Try[A] = ???
-    extension [A](m: Try[A]) 
+    override def unit[A](value: A): Try[A] = success(value)
+    extension [A](m: Try[A])
 
-      override def flatMap[B](f: A => Try[B]): Try[B] = ??? 
+      override def flatMap[B](f: A => Try[B]): Try[B] = m match
+        case TryImpl.Success(value) => f(value)
+        case TryImpl.Failure(exception) => TryImpl.Failure(exception)
       
 @main def main: Unit = 
   import Ex6TryModel.*
