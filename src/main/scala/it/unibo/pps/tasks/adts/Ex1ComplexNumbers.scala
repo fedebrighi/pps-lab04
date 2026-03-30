c83e77b92e32e2dc2d1553f0527fef62075aa4bc
Federico Brighi <federico.brighi2@studio.unibo.it.>
2026-03-30 13:31:08 +0200

    Refactored Task1: used "record.membername" syntax

---
+++ src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumbers.scala
@@ -0,0 +1,43 @@
+package it.unibo.pps.tasks.adts
+
+/*  Exercise 1: 
+ *  Complete the implementation of ComplexADT trait below, so that it passes
+ *  the test in ComplexTest.
+ */
+
+object Ex1ComplexNumbers:
+
+  trait ComplexADT:
+    type Complex
+    def complex(re: Double, im: Double): Complex
+    extension (complex: Complex)
+      def re(): Double
+      def im(): Double
+      def sum(other: Complex): Complex
+      def subtract(other: Complex): Complex
+      def asString(): String
+
+  object BasicComplexADT extends ComplexADT:
+
+    case class ComplexImpl(realPart: Double, imaginaryPart: Double)
+
+    // Change assignment below: should probably define a case class and use it?
+
+    opaque type Complex = ComplexImpl
+
+    def complex(re: Double, im: Double): Complex = ComplexImpl(re,im)
+    extension (complex: Complex)
+      def re(): Double = complex match
+        case ComplexImpl(re,_) => re
+      def im(): Double = complex match
+        case ComplexImpl(_, im) => im
+      def sum(other: Complex): Complex = (complex, other) match
+        case (ComplexImpl(re1, im1), ComplexImpl(re2, im2)) => ComplexImpl(re1 + re2, im1 + im2)
+      def subtract(other: Complex): Complex = (complex, other) match
+        case (ComplexImpl(re1, im1), ComplexImpl(re2, im2)) => ComplexImpl(re1 - re2, im1 - im2)
+      def asString(): String = complex match
+        case ComplexImpl(re, im) if im == 0 => re + ""
+        case ComplexImpl(re, im) if re != 0 && im < 0 => re+ " - " + -im + "i"
+        case ComplexImpl(re, im) if re == 0 => im + "i"
+        case ComplexImpl(re, im) => re + " + " + im + "i"
+          
